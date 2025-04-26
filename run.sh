fff9045e8b237d00dfa52fe2a5be1de3792b87d0
XueTianci <xuetianci99@gmail.com>
2025-04-25 20:23:03 -0400

    update

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-python -m test.run_single_tests --test_results_id first_300_tests
+# #!/bin/bash
+
+# while true; do
+#     python -m test.run_tests --min_task_index 0 --max_task_index 300 --test_results_id first_300_tests 
+#     echo "wait 5 seconds"
+#     sleep 5
+# done
+
+# # python -m test.run_single_tests --test_results_id first_300_tests
